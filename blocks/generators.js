e68fb7d8fc5338817ccd4a60322e62565da83c61
Nawa phansaen <79089602+nawaphan123@users.noreply.github.com>
2024-09-27 17:09:28 +0700

    Update generators.js
    update import that motor need 
    change from motor import Motor to from ncheap import NCheap,Motor,Oled,Mpu6050

--- blocks/generators.js
+++ blocks/generators.js
@@ -7,31 +7,31 @@ Blockly.Python['setUp'] = function(block) {
 
 /*-------------------------------------Motor-------------------------------------------------------*/
 Blockly.Python['motor_forward'] = function(block) {
-  Blockly.Python.definitions_['import_motor'] = 'from motor import Motor';
+  Blockly.Python.definitions_['import_motor'] = 'from ncheap import NCheap,Motor,Oled,Mpu6050';
   var speed = Blockly.Python.valueToCode(block, 'fd_speed', Blockly.Python.ORDER_ATOMIC);
   var code = `m.fd(${speed})\n`;
   return code;
 };
 Blockly.Python['motor_backward'] = function(block) {
-  Blockly.Python.definitions_['import_motor'] = 'from motor import Motor';
+  Blockly.Python.definitions_['import_motor'] = 'from ncheap import NCheap,Motor,Oled,Mpu6050';
   var speed = Blockly.Python.valueToCode(block, 'bk_speed', Blockly.Python.ORDER_ATOMIC);
   var code = `m.fd(${-speed})\n`;
   return code;
 };
 Blockly.Python['motor_spinLeft'] = function(block) {
-  Blockly.Python.definitions_['import_motor'] = 'from motor import Motor';
+  Blockly.Python.definitions_['import_motor'] = 'from ncheap import NCheap,Motor,Oled,Mpu6050';
   var speed = Blockly.Python.valueToCode(block, 'sl_speed', Blockly.Python.ORDER_ATOMIC);
   var code = `m.sl(${speed})\n`;
   return code;
 };
 Blockly.Python['motor_spinRight'] = function(block) {
-  Blockly.Python.definitions_['import_motor'] = 'from motor import Motor';
+  Blockly.Python.definitions_['import_motor'] = 'from ncheap import NCheap,Motor,Oled,Mpu6050';
   var speed = Blockly.Python.valueToCode(block, 'sr_speed', Blockly.Python.ORDER_ATOMIC);
   var code = `m.sr(${speed})\n`;
   return code;
 };
 Blockly.Python['motor_stop'] = function(block) {
-  Blockly.Python.definitions_['import_motor'] = 'from motor import Motor';
+  Blockly.Python.definitions_['import_motor'] = 'from ncheap import NCheap,Motor,Oled,Mpu6050';
   var code = `m.stop()\n`;
   return code;
 };
